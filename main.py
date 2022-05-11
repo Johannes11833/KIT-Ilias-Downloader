7b596168fe6f5140e29cb3c30d46e2c0bc203235
Johannes Gundlach <j.gundlach@me.com>
2022-05-11 11:06:39 +0200

    Added auto setup scripts for rust and the cli ilias download tool.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import logging
 import os
 import subprocess
+from pathlib import Path
 from threading import Event
 from typing import Dict
 
@@ -41,12 +42,16 @@ def download_ilias_data():
     logging.info("Starting ilias download...")
 
     # create the ilias downloader command
-    command = f"./KIT-ILIAS-downloader" \
-              f" -U {config['ILIAS_DOWNLOADER_USER_NAME']}" \
-              f" -P \"{config['ILIAS_DOWNLOADER_PASSWORD']}\"" \
-              f" -o \"output\"" \
-              f" --jobs  {config['ILIAS_DOWNLOADER_JOBS']}" \
-              f" --rate {config['ILIAS_DOWNLOADER_RATE']}"
+    if unix_like():
+        command = f"~/.cargo/bin/KIT-ILIAS-downloader"
+    else:
+        command = f"KIT-ILIAS-downloader"
+
+    command += f" -U {config['ILIAS_DOWNLOADER_USER_NAME']}" \
+               f" -P \"{config['ILIAS_DOWNLOADER_PASSWORD']}\"" \
+               f" -o \"output\"" \
+               f" --jobs  {config['ILIAS_DOWNLOADER_JOBS']}" \
+               f" --rate {config['ILIAS_DOWNLOADER_RATE']}"
 
     if config['ILIAS_DOWNLOADER_SYNC_URL']:
         command += f" --sync-url \"{config['ILIAS_DOWNLOADER_SYNC_URL']}\""
@@ -64,11 +69,55 @@ def download_ilias_data():
                         'If on linux/mac run: \n\tchmod +x KIT-ILIAS-downloader\nto make the downloader executable.')
 
 
+def unix_like() -> bool:
+    import sys
+    if 'win' in sys.platform:
+        return False
+    else:
+        return True
+
+
+def setup_ilias_downloader():
+    from shutil import which
+    if unix_like() and which(f'{Path.home()}/.cargo/bin/KIT-ILIAS-downloader') is None:
+        options = ['yes', 'no']
+        i = show_selection_dialog(options, ['y', 'n'],
+                                  f'The KIT-ILIAS-downloader is not installed on this system. Auto install it?')
+
+        if i == 'y':
+            if which('cargo') is None:
+                # install rust first
+
+                logging.info('Rust is not installed. Trying to auto install it...')
+                output = subprocess.run("curl https://sh.rustup.rs -sSf | sh -s -- -y", shell=True)
+
+                if output.returncode == 0:
+                    logging.info('Rust was successfully installed.')
+                else:
+                    raise Exception('Failed to auto install rust. '
+                                    'Please try installing it manually at: https://www.rust-lang.org/tools/install')
+
+            # install the ilias downlaoder cli tool
+            output = subprocess.run("cargo install --all-features "
+                                    "--git 'https://github.com/FliegendeWurst/KIT-ILIAS-downloader' --branch stable",
+                                    shell=True)
+
+            if output.returncode == 0:
+                logging.info('The KIT-ILIAS-downloader CLI tool was successfully installed.')
+            else:
+                raise Exception('Failed to install the KIT-ILIAS-downloader CLI tool.'
+                                'Please try manually installing it at: '
+                                'https://github.com/fliegendewurst/kit-ilias-downloader')
+
+        else:
+            quit()
+
+
 def setup_rclone():
     # check if rclone is installed
     from shutil import which
     if which('rclone') is None:
-        raise Exception('Rclone is not installed on this system. It is need to backup the files to the cloud!'
+        raise Exception('Rclone is not installed on this system. It is needed to backup the files to the cloud!'
                         'Install it here: https://rclone.org/')
 
     r_name = config['ILIAS_DOWNLOADER_RCLONE_REMOTE_NAME']
@@ -146,9 +195,12 @@ if __name__ == '__main__':
     elif not config['ILIAS_DOWNLOADER_PASSWORD']:
         logging.error('The Ilias password has not been set')
     else:
-        set_up_complete = False
+
+        # set up the ilias downloader
+        setup_ilias_downloader()
 
         # set up a cloud provider
+        set_up_complete = False
         while not set_up_complete:
             set_up_complete = setup_rclone()
 
@@ -163,6 +215,8 @@ if __name__ == '__main__':
         for t in upload_times:
             ts.add(Task(t, download_ilias_data))
         ts.start()
+
+        download_ilias_data()
 
         # wait until manually stopped
         Event().wait()
